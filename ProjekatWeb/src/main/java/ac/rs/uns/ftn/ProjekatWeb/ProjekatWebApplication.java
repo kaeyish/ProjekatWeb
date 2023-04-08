d3c78a1b8cee5fd898294e1571bcfee95f5bd59b
Dusanka Ignjatovic <ignjatovicdusanka02@gmail.com>
2023-04-08 23:36:24 +0200

    osnova projekta

---
+++ ProjekatWeb/src/main/java/ac/rs/uns/ftn/ProjekatWeb/ProjekatWebApplication.java
@@ -0,0 +1,13 @@
+package ac.rs.uns.ftn.ProjekatWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatWebApplication.class, args);
+	}
+
+}
